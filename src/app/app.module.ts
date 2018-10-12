bb3bd2a75b79811626e7ab017d3dec8b8cdd7f1a
RobertKopec <kopecrobert@op.pl>
2018-10-12 18:17:27 +0200

    Partially implemented and styled basic components: navbar, input, select and button. Added Fonts Awesome to project. Imported few icons for navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+import { SelectComponent } from './components/select/select.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library} from '@fortawesome/fontawesome-svg-core';
+// tslint:disable-next-line:max-line-length
+import { faTh, faThList, faMapMarkedAlt, faPlus, faFilter, faStar, faSignInAlt, faSignOutAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTh, faThList, faMapMarkedAlt, faPlus, faFilter, faStar, faSignInAlt, faSignOutAlt, faSearch);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButtonComponent,
+    InputComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
